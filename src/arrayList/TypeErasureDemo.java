07245a958f1b460ab1263e62d8d89fdbd79beb7b
Mianxiong <757134184@qq.com>
2023-03-02 20:47:29 +0800

    类型擦除

---
+++ src/arrayList/TypeErasureDemo.java
@@ -0,0 +1,21 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class TypeErasureDemo {
+    public static void main(String[] args) {
+        //类型擦除 Type erasure -> 改变javac编译器，不需要改字节码，也不需要改虚拟机
+        ArrayList arrayList = new ArrayList();
+        // ArrayList<String> -> ArrayList
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        // ArrayList<Integer> -> ArrayList 裸类型 Raw Type
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(1);
+        // integerArrayList.add("abd");
+
+        arrayList = stringArrayList;
+        arrayList = integerArrayList;
+        arrayList.add("abc");
+        arrayList.add(true);
+    }
+}
